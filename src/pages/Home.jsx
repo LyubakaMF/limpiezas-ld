227df2d1413145697537c533c7a382f1eebaf592
Любомир Димитров <lyubomir_dr_dimitrov@abv.bg>
2026-03-18 02:09:55 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/home/HeroSection';
 import ServicesPreview from '../components/home/ServicesPreview';
 import StatsSection from '../components/home/StatsSection';
 import TestimonialsSection from '../components/home/TestimonialsSection';
+import FaqSection from '../components/home/FaqSection';
 import CTASection from '../components/home/CTASection';
 
 export default function Home() {
